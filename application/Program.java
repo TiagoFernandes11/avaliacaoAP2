7a7065c73884fe1029e1cadf2506e1c7ca35b9b8
Tiago Fernandes <tiagofernandesribeiro@yahoo.com.br>
2022-10-12 08:29:52 -0300

    Fibonacci Sequence implementation

--- application/Program.java
+++ application/Program.java
@@ -1,9 +1,10 @@
 package application;
 
+import functions.FibonacciSequence;
 import functions.RegressiveCount;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println(new RegressiveCount(5));
+        System.out.println(new FibonacciSequence(10));
     }
 }
--- functions/FibonacciSequence.java
+++ functions/FibonacciSequence.java
@@ -2,9 +2,35 @@ package functions;
 
 public class FibonacciSequence {
     private int number;
+    private String resultado = "1, ";
 
     public FibonacciSequence(int number) {
         this.number = number;
+        
+    }
+
+    private String fibonacciGenerator(int number){
+        int anteriorValue = 0;
+        int currentValue = 1;
+        int result = 0;
+        int size = number;
+        return fibonacciGenerator(size, currentValue, anteriorValue, result, resultado);
+    }
+    private String fibonacciGenerator(int size, int number, int anteriorValue, int result, String resultado){
+        if(size>0){
+            result = number + anteriorValue;
+            anteriorValue = number;
+            number = result;
+            size--;
+            resultado += result + ", ";
+            return fibonacciGenerator(size, number, anteriorValue, result, resultado);
+        }
+        return resultado;
+    }
+
+    @Override
+    public String toString() {
+        return fibonacciGenerator(number);
     }
 
     
