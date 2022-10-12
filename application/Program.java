3cd69564465f8169c0ff1aad3128d18861d1aee0
Tiago Fernandes <tiagofernandesribeiro@yahoo.com.br>
2022-10-12 18:25:58 -0300

    Program full implemetation

--- application/Program.java
+++ application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 import entities.IntersectionArrayInt;
 import functions.FibonacciSequence;
 import functions.RandomIntegerArrayGen;
@@ -8,12 +10,21 @@ import input.IntInput;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Contagem regressiva: \n\n");
+        System.out.println("Digite o valor a ser contado regressivamente: ");
+        System.out.println(new RegressiveCount(sc.nextInt()) + "\n\n");
 
+        System.out.println("Sequencia de Fibonacci: \n\n");
+        System.out.println("Digite o valor do tamanho do vetor de Fibonacci: ");
+        System.out.println(new FibonacciSequence(sc.nextInt()) + "\n\n");
 
+        System.out.println("Input desconsiderando repetições: \n\n");
+        System.out.println("Digite o valor do tamanho do vetor de Entradas: ");
+        System.out.println(new IntInput(sc.nextInt()) + "\n\n");
 
-        
-        System.out.println("Gerador do vetor intersecção");
+        System.out.println("Gerador do vetor intersecção: \n\n");
         RandomIntegerArrayGen randomArray = new RandomIntegerArrayGen(10);
         IntInput inputArray = new IntInput(5);
 
